0480caed85cc24593b3c9fe2a099f16d2cb2cf26
Gjrmadhuri <84589135+Gjrmadhuri@users.noreply.github.com>
2021-05-30 23:27:04 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -6,7 +6,7 @@ def wallis(n):
     p=1
     for i in range(1, n+1):
         p = p*(4*i*i/(4*i*i-1))
-        return (p*2)
+    return (p*2)
 def monte_carlo(n):
     count=0
     for i in range(1, n+1):
